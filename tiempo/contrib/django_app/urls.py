3ba8e8dd5c1ae68b8a12505969907cb6beebbc4f
anthonyalmarza <anthony.almarza@gmail.com>
2016-01-12 14:55:21 -0500

    fixes dot path reference to tiempo.contrib.django_app

--- tiempo/contrib/django_app/urls.py
+++ tiempo/contrib/django_app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from tiempo.contrib.django_app.views import TiempoKiosk
 
 urlpatterns = patterns(
-    'tiempo.contrib.django.views',
+    'tiempo.contrib.django_app.views',
     url(r'^$', 'dashboard', name='tiempo_dashboard'),
     url(r'^kiosk/$', TiempoKiosk.as_view(), name="Tiempo Live Kiosk"),
     url(r'^history/$', TiempoKiosk.as_view(), name="Tiempo Task History"),
